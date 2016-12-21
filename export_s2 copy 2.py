800a4021cd8e492fea053598c516e06dd9837c2a
krubottom <karl.rubottom@gmail.com>
2016-12-20 20:02:44 -0700

    changing import format

--- export_s2 copy 2.py
+++ export_s2 copy 2.py
@@ -11,35 +11,24 @@ cur = conn.cursor()
 cur.execute("""SELECT * from person""")
 rows = cur.fetchall()
 
-print ("COMMAND,LASTNAME,FIRSTNAME,CREDENTIALS,ACCESSLEVELS")
+print ("APICommand,EncodedNum1,CardFormat1,FirstName,LastName,Accesslevel1,PersonID,PictureFile,PIN")
 
 for row in rows:
     printStr = 0
-    strCommand = "AddPerson,"
-    strFirstName = row[3].replace(',', '') + ","
-    strLastName = row[5].replace(',', '') + ","
-    strCredentials = ""
-
+    userStr = "AddPerson,"
     fobcur = conn.cursor()
     fobcur.execute("SELECT * from novuskey WHERE ownerid = %s", (row[0],))
 
     fob = fobcur.fetchall()
-    i = 0
+
     for sub_fob in fob:
-        i = i +1
-        if i == 1:
-            strCredentials = "{"
         # print sub_fob
         printStr = 1
         if sub_fob != None:
             fob_fc = sub_fob[3].split(":")[1].split("-")[0]
             fob_id = sub_fob[3].split(":")[1].split("-")[1]
         # print "First Name: ", row[3], "\nLast Name: ", row[5], "\nCard FC: ", fob_fc, "\nCard Number: ", fob_id
-            strCredentials = strCredentials + fob_id + "~" + fob_id + "~" + fob_fc + "~Active~~"
-        if len(fob) > i:
-            strCredentials = strCredentials + "|"
-        if i == len(fob):
-            strCredentials = strCredentials + "}"
+            userStr = userStr + fob_fc + "," + fob_id + ","
         # accur = conn.cursor()
         # accur.execute("SELECT * from usergroupmember where memberid = %s", (row[0],))
         # ac_group = accur.fetchall()
@@ -52,7 +41,8 @@ for row in rows:
         #         print "".join('%s'%x for x in grp_name[0][1] )
         #     print "\n"
 
+    userStr = userStr + row[3] + "," #first name
+    userStr = userStr + row[5] + ",,,," #last name and padding
 
-
-    if printStr == 1:
-        print strCommand + strFirstName + strLastName + strCredentials
+    if printStr == 1 and row[3] != None:
+        print userStr
